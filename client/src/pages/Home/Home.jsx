e9c4f95efea99fc93012bef806d1bff2eb9a9ac0
Himosh <121594264+Himosh@users.noreply.github.com>
2024-06-03 23:51:48 +0530

    #4.7, Redirect to the home page on logout

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import Navbar from '../../components/navbar/Navbar'
-import Hero from '../../components/hero/Hero'
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import authService from '../../services/authService';
+import Navbar from '../../components/navbar/Navbar';
+import Hero from '../../components/hero/Hero';
 
 function Home() {
+
   return (
     <div className="home-container">
-        <Navbar />
-        <Hero />
+      <Navbar />
+      <Hero />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
