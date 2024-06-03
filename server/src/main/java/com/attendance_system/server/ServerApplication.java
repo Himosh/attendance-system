e168c620d12c71d0e430c37f3f86b90401b2347e
Himosh <121594264+Himosh@users.noreply.github.com>
2024-06-03 10:37:00 +0530

    #1, initiated server code

---
+++ server/src/main/java/com/attendance_system/server/ServerApplication.java
@@ -0,0 +1,20 @@
+package com.attendance_system.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public static String welocme(){
+		return "hello";
+	}
+}
