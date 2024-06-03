ff79624f506950a6accf7c3df598e51d1f80a697
Himosh <121594264+Himosh@users.noreply.github.com>
2024-06-03 23:50:43 +0530

    #4.6, Successfully log in the user and redirect to the home page.

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -1,20 +1,50 @@
-import React from 'react'
-import './Hero.css'
-import Background from '../../assets/Background.svg'
+import React, { useState, useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import authService from '../../services/authService';
+import './Hero.css';
+import Background from '../../assets/Background.svg';
 
 function Hero() {
+  const [currentUser, setCurrentUser] = useState(undefined);
+  const navigate = useNavigate();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const logOut = () => {
+    authService.logout();
+    setCurrentUser(undefined);
+    setIsAuthenticated(true);
+    navigate('/');
+  };
+
+  useEffect(() => {
+    const user = authService.getCurrentUser();
+    if (user) {
+      setCurrentUser(user);
+    } else {
+      setCurrentUser(null);
+    }
+  }, []);
+
   return (
     <div className="hero-container">
       <div className="container-left">
-        <h1>It’s seamless, fast & simple. </h1>
+        <h1>{!isAuthenticated ? `Welcome back` : 'It’s seamless, fast & simple.'}</h1>
         <p>Attendance System is a modern solution for tracking attendance. It is a fast, reliable and simple way to track attendance.</p>
-        <button>Login</button>
+        {!isAuthenticated ? (
+          <button className='navbar-login' onClick={logOut}>
+            <Link style={{ textDecoration: 'none', color: '#626262' }} to='/'>Logout</Link>
+          </button>
+        ) : (
+          <button className='navbar-login'>
+            <Link style={{ textDecoration: 'none', color: '#626262' }} to='/login'>Login</Link>
+          </button>
+        )}
       </div>
       <div className="container-right">
-        <img src={Background} alt="hero" /> 
+        <img src={Background} alt="hero" />
       </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
