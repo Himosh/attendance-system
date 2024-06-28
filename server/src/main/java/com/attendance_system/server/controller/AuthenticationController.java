b45aaa3675d405b045ed67bc39ccf79e0e38e9d8
Himosh <121594264+Himosh@users.noreply.github.com>
2024-06-28 09:12:53 +0530

    cross origin

--- server/src/main/java/com/attendance_system/server/controller/AuthenticationController.java
+++ server/src/main/java/com/attendance_system/server/controller/AuthenticationController.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3001")
 @RestController
 @RequestMapping("/api")
 public class AuthenticationController {
